d5305783dd671b0d97e862382f4d510d28bc6cf5
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-03-20 21:57:39 -0600

    added front end react to display thoughts on home page

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const { ApolloServer } = require("apollo-server-express");
 // importing middleware
 const { authMiddleware } = require("./utils/auth");
 
+const path = require("path");
+
 //import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -35,6 +37,14 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
